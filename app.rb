9053c3b926a2c6dcbc8d7ca90b32b872c7aff6e1
katherineabuhadal <katherine8@verizon.net>
2014-06-14 13:07:42 -0400

    add/delete deck

--- app.rb
+++ app.rb
@@ -10,22 +10,57 @@ class Card < ActiveRecord::Base
 end
 
 class Deck < ActiveRecord::Base
+	has_many :cards
 end
 
 get "/" do
-	
 	@decks = Deck.all
 	erb :index
 end
 
-
-
+get "/new_deck" do
+	erb :new_deck
+end
 
 get "/decks/:id" do
 	@deck = Deck.find(params[:id])
-	@cards = Card.where(deck_id: @deck_id)
+	@card = Card.where(deck_id: @deck.id).sample
 
 	erb :show
 end
+
+post "/decks" do
+	deck = Deck.create(params[:deck])
+
+	redirect "/decks/#{deck.id}"
+end
+
+get "/cards/:id/edit" do
+	@deck = Deck.find(params[:id])
+	@cards = Card.where(deck_id: @deck.id)
+	erb :new_card
+end
+
+post "/cards" do
+	card = Card.create(params[:card])
+	redirect "/cards/#{card.id}"
+	
+end
+
+delete "/decks/:id" do
+	deck = Deck.find(params[:id]) 
+	deck.destroy
+
+	redirect "/"
+end
+
+
+
+
+
+
+
+
+
 
 
